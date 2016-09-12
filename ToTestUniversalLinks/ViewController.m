9e42760192cf7efb7ffee292e185e39577cc88a7
Kishore Thejasvi <kishore@bigbasket.com>
2016-09-12 10:19:06 +0530

    Final commit.

--- ToTestUniversalLinks/ViewController.m
+++ ToTestUniversalLinks/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textfield;
+
+- (IBAction)didTapOnView:(id)sender;
+
+- (IBAction)didSelectGo:(id)sender;
 
 @end
 
@@ -16,12 +22,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.textfield.delegate = self;
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://qa2.bigbasket.com/payment/wallet/"]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)didTapOnView:(id)sender
+{
+    [self.view endEditing:YES];
+}
+
+- (IBAction)didSelectGo:(id)sender
+{
+    [self openUniversalLink];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.view endEditing:YES];
+    return YES;
+}
+
+- (void)openUniversalLink
+{
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://qa2.bigbasket.com/payment/wallet/"]];
+    
+    if ([self.textfield.text length] > 0)
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.textfield.text]];
+    }
 }
 
 @end
